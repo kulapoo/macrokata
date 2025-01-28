a6ee3070f84798fe2d59086a446ac55af565a0aa
kulapoo <kulaskulapoo@gmail.com>
2025-01-28 23:43:24 +0800

    update

--- exercises/04_expression_variables/main.rs
+++ exercises/04_expression_variables/main.rs
@@ -7,6 +7,15 @@ fn print_result(num: i32) {
 // TODO: create `math!()` macro.
 
 ////////// DO NOT CHANGE BELOW HERE /////////
+macro_rules! math {
+    ($lhs: expr, plus, $rhs: expr) => {
+        $lhs + $rhs
+    };
+    (square $arg: expr) => {
+        $arg * $arg
+    };
+}
+
 
 fn main() {
     let var = 5;
