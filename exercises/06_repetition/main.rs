cb22d1780587017ffde97e07997c3192166557b8
kulapoo <kulaskulapoo@gmail.com>
2025-01-31 02:23:20 +0800

    solve repitition

--- exercises/06_repetition/main.rs
+++ exercises/06_repetition/main.rs
@@ -5,11 +5,20 @@ fn print_success() {
 ////////// DO NOT CHANGE ABOVE HERE /////////
 
 // TODO: create `if_any!()` macro.
+macro_rules! if_any {
+    ($($predicate: expr),+;$block: block) => {
 
+        if $($predicate)||+ $block
+    };
+}
 ////////// DO NOT CHANGE BELOW HERE /////////
 
+
+
 fn main() {
+
     if_any!(false, 0 == 1, true; {
         print_success();
-    })
+    });
+
 }
